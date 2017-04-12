4151699d8e447ffc3c901c4523995d7027b7a9e8
arnb13 <arnb13@gmail.com>
2017-04-12 23:17:18 +0600

    @D platformer Unity Practice

---
+++ Assets/Code/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Vector3 velocity;
+    public float timeX = 0.05f;
+    public float timeY = 0.05f;
+
+    public GameObject player;
+     
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, timeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, timeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ Assets/Code/walking.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class walking : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Animator a;
+
+    public float speed = 10;
+    public static float velX = 0;
+    public static float velY = 0;
+    Vector2 max = new Vector2(3, 5);
+    public bool j = false;
+    
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        a = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velX = 0;
+        velY = 0;
+
+
+        float absX = Mathf.Abs(rb.velocity.x);
+
+
+        
+
+        if (CrossPlatformInputManager.GetAxis("Horizontal") > 0)
+        {
+            if(absX < max.x)
+            {
+                velX = speed;
+            }
+            transform.localScale = new Vector3(1, 1, 1);
+            a.SetInteger("anim", 1);
+        } else if(CrossPlatformInputManager.GetAxis("Horizontal") < 0)
+        {
+            if (absX < max.x)
+            {
+                velX = -speed;
+            }
+            transform.localScale = new Vector3(-1, 1, 1);
+            a.SetInteger("anim", 1);
+        } else
+        {
+            a.SetInteger("anim", 0);
+        }
+        rb.AddForce(new Vector2(velX, velY));
+        
+    }
+    public void jump0()
+    {
+
+
+        float absY = Mathf.Abs(rb.velocity.y);
+        if (absY < 0.2)
+        {
+            rb.velocity = new Vector2(0, 7);
+        }
+
+    }
+    public void jump1()
+    {
+        velY = 0;
+        
+    }
+}
